50274318ba4dfcfc35b66dbff02793a7efdb4475
Iheb Inoubli <ihebinoubli@outlook.com>
2026-07-23 11:47:34 +0200

    sbb to sbb correction

--- interop/sbb-wrapper/tests/sbb_safretl_smoke_test.c
+++ interop/sbb-wrapper/tests/sbb_safretl_smoke_test.c
@@ -2,21 +2,33 @@
 #include "udp_transport.h"
 
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
 #ifndef SBB_WRAPPER_HAS_SBB_REDL
+    (void)argc;
+    (void)argv;
     puts("sbb_safretl_smoke_test: skipped because SBB_ROOT was not provided");
     return 0;
 #else
-    SbbWrapperUdpConfig udp_config = {
-        .remote_ip = "127.0.0.1",
-        .channel0 = {.local_port = 39300u, .remote_port = 39301u},
-        .channel1 = {.local_port = 39301u, .remote_port = 39300u},
-        .trace = 0,
-    };
+    SbbWrapperUdpConfig udp_config;
     SbbEndpoint endpoint;
+    SbbEndpointRole role;
     radef_RaStaReturnCode result;
+
+    if (argc != 2 || (strcmp(argv[1], "active") != 0 && strcmp(argv[1], "passive") != 0)) {
+        puts("usage: sbb_safretl_smoke_test <active|passive>");
+        return 2;
+    }
+
+    role = strcmp(argv[1], "active") == 0 ? SBB_ENDPOINT_ROLE_ACTIVE : SBB_ENDPOINT_ROLE_PASSIVE;
+    udp_config.remote_ip = "127.0.0.1";
+    udp_config.channel0.local_port = role == SBB_ENDPOINT_ROLE_ACTIVE ? 39300u : 39310u;
+    udp_config.channel0.remote_port = role == SBB_ENDPOINT_ROLE_ACTIVE ? 39301u : 39311u;
+    udp_config.channel1.local_port = role == SBB_ENDPOINT_ROLE_ACTIVE ? 39301u : 39311u;
+    udp_config.channel1.remote_port = role == SBB_ENDPOINT_ROLE_ACTIVE ? 39300u : 39310u;
+    udp_config.trace = 0;
 
     if (sbb_wrapper_udp_init(&udp_config) != 0) {
         puts("sbb_safretl_smoke_test: UDP init failed");
@@ -24,7 +36,7 @@ int main(void)
     }
 
     redtri_Init();
-    sbb_endpoint_configure(&endpoint, SBB_ENDPOINT_ROLE_ACTIVE, 0);
+    sbb_endpoint_configure(&endpoint, role, 0);
 
     result = sbb_endpoint_init(&endpoint);
     if (result != radef_kNoError) {
@@ -55,7 +67,7 @@ int main(void)
     }
 
     sbb_wrapper_udp_close();
-    puts("sbb_safretl_smoke_test: passed");
+    printf("sbb_safretl_smoke_test: %s startup passed\n", argv[1]);
     return 0;
 #endif
 }
